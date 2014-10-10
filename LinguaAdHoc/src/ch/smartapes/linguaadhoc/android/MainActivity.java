9aaba992361ad0b026bc9119bb57458e4f1e5a2f
Fabian Tschopp <tschopfa@student.ethz.ch>
2014-10-11 01:18:07 +0200

    POI api

--- LinguaAdHoc/src/ch/smartapes/linguaadhoc/android/MainActivity.java
+++ LinguaAdHoc/src/ch/smartapes/linguaadhoc/android/MainActivity.java
@@ -1,5 +1,9 @@
-package ch.smartapes.linguaadhoc;
+package ch.smartapes.linguaadhoc.android;
 
+import ch.smartapes.linguaadhoc.R;
+import ch.smartapes.linguaadhoc.R.id;
+import ch.smartapes.linguaadhoc.R.layout;
+import ch.smartapes.linguaadhoc.R.menu;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,16 +21,12 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
